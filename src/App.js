a3aed27d88bb30f7630abe88dcb22522575b1579
shawn-phy <philipoyuga14@gmail.com>
2024-06-06 20:56:40 +0300

    feat: changed the app route to serve events fetched from endpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Events from './Events';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Events App</h1>
       </header>
+      <main>
+        <Events />
+      </main>
     </div>
   );
 }

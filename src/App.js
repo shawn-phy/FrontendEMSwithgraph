2239d649fa72adb7566e1f3b77d098669409576e
shawn-phy <philipoyuga14@gmail.com>
2024-06-06 23:16:46 +0300

    feat:adds tailwind base classes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>My Events App</h1>
+        <h1 className="text-3xl font-bold underline">My Events App</h1>
       </header>
       <main>
         <Events />
